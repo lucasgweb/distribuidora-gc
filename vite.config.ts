a27cbdf550088f590fda9c712ea7096455411e6f
Lucas Godoy <lucasgg.tech@gmail.com>
2025-04-12 10:07:21 -0300

    Add global type definitions for Vite PWA client

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,55 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss(), VitePWA({
+        workbox: {
+          sourcemap: true,
+        },
+        registerType: 'autoUpdate',
+        includeAssets: ['apple-touch-icon.png'],
+        manifest: {
+          name: 'Distribuidora GC',
+          short_name: 'Distribuidora GC',
+          description: 'Distribuidora GC',
+          theme_color: '#ffffff',
+          start_url: '/',
+          icons: [
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
+          share_target: {
+            action: '/share',
+            method: 'POST',
+            enctype: 'multipart/form-data',
+            params: {
+              files: [
+                {
+                  name: 'file',
+                  accept: ['*/*'],
+                },
+              ],
+            },
+          },
+        },
+      }),],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
